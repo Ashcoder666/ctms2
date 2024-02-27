3dfc4d20130d218c52bc6a062888a2fa541425e8
ashir <ashir@metrictreelabs.com>
2024-02-27 14:47:29 +0530

    added user model  for auth

---
+++ src/modules/authModules/authModel.ts
@@ -0,0 +1,41 @@
+import mongoose, { Types, Schema, Document } from "mongoose";
+
+enum Roles {
+  manger = "MANAGER",
+  developer = "DEVELOPER",
+  qa = "QA",
+  uiux = "UIUX",
+  others = "OTHERS",
+}
+
+interface Iuser extends Document {
+  _id: Types.ObjectId;
+  fullName: string;
+  role: Roles;
+  email: string;
+  company_id: Types.ObjectId;
+}
+
+const userSchema: Schema<Iuser> = new Schema<Iuser>({
+  fullName: {
+    type: "string",
+    required: true,
+  },
+  role: {
+    type: "string",
+    enum: ["MANAGER", "DEVELOPER", "QA", "UIUX", "OTHERS"],
+    required: true,
+  },
+  email: {
+    type: "string",
+    required: true,
+  },
+  company_id: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+});
+
+const userModel = mongoose.model<Iuser>("users", userSchema);
+
+export default userModel;
